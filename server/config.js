6c0d148193d2728732874ceaa566f6066786d15b
JoshFouchey <JoshFouchey@gmail.com>
2017-07-26 12:49:32 -0400

    Ignore config

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 ServiceConfiguration.configurations.insert({
     service: "google",
-    clientId:"secret",
-    secret: "secret"
+    clientId:"244619248682-o9j5lbj64fghfr7g137epq6har9u51m6.apps.googleusercontent.com",
+    secret: "oXJVXYBJWfcnbPAhgPlHSJft"
 });
